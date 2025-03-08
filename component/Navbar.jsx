3bdffda0fe157d5d16696ac5703a65d4dfd68e97
abhi <rajidiabhinavreddy@gmail.com>
2025-03-09 01:20:21 +0530

    done with nav bar

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,1 +1,51 @@
-rafce
+
+import '../component/Navbar.css'
+import { useState } from 'react';
+
+
+const  Navbar = () => {
+    const [open , setOpen] = useState(false);
+    return (
+        <div id='mainbar' className='md:h-20 w-full'>
+            {/* Logo */}
+            <div id='logo'>
+                <img src="/logo.png" alt="" />
+                <span>AbhiLog</span>
+            </div>
+            {/* mobileMEnu */}
+            <div id='mobileMenu'>
+                <div id='menubarMobile' onClick={() => setOpen(!open)}>
+                    {open ? "╳": "☰"}
+                </div>
+                {/* <div id="menuList">
+                    menu
+                </div> */}
+            </div>
+            {/* desktopMenu */}
+            <div className="" id='desktopMenu'>
+                <div id="first">
+                <a href="/">Home</a>
+                <hr />               
+                </div>
+                <div id="second">
+                <a href="/">About</a>
+                <hr />
+                </div>
+                <div id="third">
+                <a href="/">Trending🔥</a>
+                <hr />
+                </div>
+                <div id="fourth">
+                <a href="/">Most Popular</a>
+                <hr />
+                </div>
+                
+                <a href="">
+                    <button>Login</button>
+                </a>
+            </div>
+
+        </div>
+    );
+};
+export default Navbar
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+// import React from 'react'
+import Navbar from '../component/Navbar'
+
+
 const App = () => {
   return (
-    <div className='px-4 md:px-8 lg:px-16 lxl:px-32 2xl:px-64'>Hello World</div>
+    <>
+    <div className='px-4 md:px-8 lg:px-16 lxl:px-32 2xl:px-64'>
+      {/* <Navbar/> */}
+      <Navbar/>
+      {/* <Breadcrumb/> */}
+      {/* <Introduction/> */}
+      {/* <featured posts/> */}
+      {/* <Post list/> */}
+    </div>
+    </>
   )
 }
 
