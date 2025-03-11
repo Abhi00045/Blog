e707af0291f57c299ca5b7e496f57e6a52f6164b
abhi <rajidiabhinavreddy@gmail.com>
2025-03-11 19:23:05 +0530

    added codeuim

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../main.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div id='top-home'>
+      {/* Introduction */}
+      <div id="first-top">
+         <p>Home</p>
+         <span>.</span>
+         <span id="Blogs">
+          Blogs and Articles
+         </span>
+      </div>
+    </div>
+    </>
   )
 }
 
