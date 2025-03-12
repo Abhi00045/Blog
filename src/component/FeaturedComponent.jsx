9421a03c552584ee8b1643834fd0c014c0ad41fd
abhi <rajidiabhinavreddy@gmail.com>
2025-03-13 00:55:34 +0530

    added featured components lets gooo with postlist and single post list

--- src/component/FeaturedComponent.jsx
+++ src/component/FeaturedComponent.jsx
@@ -7,7 +7,8 @@ function FeaturedComponent() {
     <>
     <div id="main-featuredComponents">
  <div id="left-featuredComponent">
-            
+
+{/* 1st one */}
                 <img src="./featured1.jpeg"alt="" />
                 <div id="left-post-one">
                     <div id="left-post-one-top">
@@ -19,18 +20,48 @@ function FeaturedComponent() {
                 </div>
             
         </div>       
-        <div id="right-featuredComponent"> 
-           
-        <img src="./featured1.jpeg"alt=""  />
+        <div id="right-featuredComponent">
+
+{/* 2nd one */}
+          <div id="right-second">
+          <img src="./featured1.jpeg"alt=""  />
                 <div id="left-post-one">
                     <div id="left-post-one-top">
-                      <p>01</p>
+                      <p>02</p>
                       <h1>Web design</h1>
                       <span>2 days ago</span>
                       </div>
                       <h1 id='main'>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>     
                 </div>
+          </div>
+
+{/* 3rd one */}
+          <div id="right-second">
+          <img src="./featured1.jpeg"alt=""  />
+                <div id="left-post-one">
+                    <div id="left-post-one-top">
+                      <p>03</p>
+                      <h1>Web design</h1>
+                      <span>2 days ago</span>
+                      </div>
+                      <h1 id='main'>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>     
                 </div>
+          </div>
+
+{/* 4th one */}
+          <div id="right-second">
+          <img src="./featured1.jpeg"alt=""  />
+                <div id="left-post-one">
+                    <div id="left-post-one-top">
+                      <p>04</p>
+                      <h1>Web design</h1>
+                      <span>2 days ago</span>
+                      </div>
+                      <h1 id='main'>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>     
+                </div>
+          </div>
+        </div>
+                
     </div>
     </>
   )
