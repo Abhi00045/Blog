7f46290740705ec8a5307148c6d974001fdeea3b
abhi <rajidiabhinavreddy@gmail.com>
2025-03-15 19:25:54 +0530

    done with single posat need to add css pure

---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Search from './Search'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+function Sidebar() {
+  return (
+   <>
+           <div id="bottom-right-menu">
+             <div id='one-author'>
+                <h3>Author</h3>
+               <div id="profile">
+               <img src="./userImg.jpeg" alt="" />
+               <span>Abhishek</span>
+               </div>
+               <div id="Bio">
+                <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+               </div>
+               <div id="Socials">
+                <img src="./facebook.svg" alt="" />
+                <img src="./instagram.svg" alt="" />
+               </div>
+             </div>
+             <div id='two-Actions'>
+                <h3>Actions</h3>
+                <div id="action-1">
+                    <img src="./bookmark.svg" alt="" />
+                    <span>Save this post</span>
+                </div>
+                <div id="action-2">
+                    <img src="./delete.svg" alt="" />
+                    <span>Delete this Post</span>
+                </div>
+             </div>
+             <div id='three-categories'>
+                <h3>Categories</h3>
+                <div id="category">
+                    <Link to="/posts"><span>All Posts</span></Link>
+                </div>
+                <div id="category">
+                    <Link to="/posts"><span>Web design</span></Link>
+                </div>
+                <div id="category">
+                    <Link to="/posts"><span>Development</span></Link>
+                </div>
+                <div id="category">
+                    <Link to="/posts"><span>Databases</span></Link>
+                </div>
+             </div>
+             <div id="search-four">
+               <Search/>
+             </div>
+           </div>
+   </>
+  )
+}
+
+export default Sidebar
--- src/component/SinglePostPage.jsx
+++ src/component/SinglePostPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Navbar.css'
+import Sidebar from './Sidebar'
 
 function SinglePostPage() {
   return (
@@ -20,6 +21,15 @@ function SinglePostPage() {
       <div id="image-top-page">
         <img src="./postImg.jpeg" alt="" />
       </div>
+    </div>
+    <div id="single-bottom-page">
+        <div id="bottom-left">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut itaque cumque illum, voluptates eligendi distinctio asperiores corporis illo recusandae modi laborum. Cum ut, mollitia in nulla distinctio doloremque unde harum laboriosam assumenda odit hic explicabo commodi iusto fuga laborum consequuntur tempora. Exercitationem fugit adipisci nihil in eos natus placeat saepe ipsa vel! Voluptatibus laborum soluta dignissimos unde nobis inventore saepe perspiciatis dolorum non quae voluptate, rem nam. Quaerat, nulla. Magnam fugit ex sunt consequatur corporis, soluta sit officiis enim dicta fugiat delectus quis iure, optio non nam ipsa labore quaerat. Totam eius perspiciatis assumenda. Temporibus impedit hic odio officiis fuga.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt delectus accusamus architecto ab nemo reiciendis iure reprehenderit enim animi velit excepturi quidem dolorem, dolorum ipsum corrupti exercitationem molestias repellendus. Tempora optio provident architecto omnis natus totam ipsa veritatis ex deleniti quam tempore quos rem error, ratione vitae neque vero fugit consequatur debitis similique quis enim. Distinctio ipsa nam itaque architecto odit quaerat, non nobis ullam, natus, impedit dolores earum ipsum voluptatem vitae. Magnam nesciunt tempore quibusdam. Hic corporis ipsum ex totam pariatur impedit nihil reiciendis necessitatibus voluptatum magnam, illo nisi ipsam harum. Numquam dicta at, ducimus laudantium necessitatibus distinctio ullam.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere eos ipsam quia voluptates vel culpa harum eius omnis! Magnam omnis et accusantium praesentium eos tempora, dignissimos porro hic consequatur harum iste corrupti sapiente magni autem non odio. Officiis rem non nihil quo recusandae laudantium mollitia quidem, quod est iure expedita.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem beatae tempora sequi iste vel eius, praesentium deserunt explicabo sit optio voluptatum, eaque, delectus voluptatem excepturi qui exercitationem rerum laborum iusto? Sapiente temporibus aliquam dicta consequatur error possimus impedit eos commodi facere delectus fugiat, molestiae sequi nemo velit. Inventore molestiae doloribus accusantium hic voluptate modi magnam. Excepturi incidunt, dolor sequi, quasi animi earum expedita tempora nisi et numquam fugiat nihil adipisci voluptatum dolorem consequuntur ad ipsam ratione molestias vero pariatur praesentium facere ipsa quod? Dignissimos nisi omnis nobis quos quasi quam tempora repellat cumque maxime, eius alias? Placeat qui cumque consectetur?</p>
+        </div>
+        <Sidebar/>
     </div>
     </>
   )
