5cac4929fa0a18e793a4e140feaaf1ebd03a0ed0
abhi <rajidiabhinavreddy@gmail.com>
2025-03-16 15:50:35 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ import express from 'express'
 import useRouter from './Routes/user.route.js'
 import postRouter from './Routes/post.route.js'
 import commentRouter from './Routes/comment.route.js'
+import connectDb from './connection/connectDb.js'
 
 const app = express()
 
 app.use("/user", useRouter);
 app.use("/post", postRouter);
 app.use("/comment", commentRouter);
+
+connectDb();
+
 
 app.listen(8171, () => {
     console.log('Server is running on port 8171');
