df3d2b48c2209954539b79668f97da55a133323b
abhi <rajidiabhinavreddy@gmail.com>
2025-05-31 15:55:48 +0530

    don t

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { Connection } from './connection.js';
+import postRouter from './routes/post.route.js';
 
 const app = express();
+
+app.use("/post",postRouter)
 
 app.listen(3010 ,()=>{
     Connection();
     console.log("server started");   
-    
 })
+
