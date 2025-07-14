433d31553f97f63c7c0fe686c169f09181b15bde
abhi <rajidiabhinavreddy@gmail.com>
2025-07-15 01:20:47 +0530

    added verfication procedure of clerk

--- server/controllers/webhook.controller.js
+++ server/controllers/webhook.controller.js
@@ -1,4 +1,30 @@
+import "dotenv/config";
+import { Webhook } from "svix";
+// import { Message } from "svix/dist/api/message";
+
 export const clerkController = (req , res)=>{
-    console.log("hii");
+    
+    const WEBHOOK_SECRET = process.env.CLERK_SECERT;
+
+    if(!WEBHOOK_SECRET){
+        throw new Error("webhook secert needed");
+    }
+
+    const payload = req.body;
+    const headers = req.headers;
+
+    const wh = new Webhook(secret);
+    let eve;
+    try {
+        eve = wh.verify(payload, headers);
+    } catch (err) {
+        res.status(400).json({
+            message:"Webhook verification failed"
+        });
+    }
+
+    // Do something with the message...
+
+    res.json({});
     
 }
--- server/routes/webhook.route.js
+++ server/routes/webhook.route.js
@@ -1,8 +1,12 @@
+import "dotenv/config";
 import express from 'express';
+
 import { clerkController } from '../controllers/webhook.controller.js';
+import bodyParser from 'body-parser';
+
 
 const router = express.Router();
 
-router.post("/webhook",clerkController);
+router.post("/webhook", bodyParser.raw({ type: 'application/json' }),clerkController);
 
-export default router;
+export default router; 
